645ae0f3466d18e6ddac833af0a7c068a99305c8
mzakirahman <mzakiriau@gmail.com>
2021-03-20 20:53:37 +0700

    Abstract Class & Interface

---
+++ lib/MonsterUcoa.dart
@@ -0,0 +1,11 @@
+import 'package:dart_1/flying_monster.dart';
+import 'package:dart_1/monster_ubur_ubur.dart';
+
+class Monsterucoa extends MonsterUburUbur implements FlyingMonster {
+  @override
+  String fly() {
+    return "Terbang terbang melayang";
+  }
+}
+
+abstract class BasketPlayer {}
